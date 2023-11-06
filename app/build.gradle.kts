33a694bd8463dd24c3c336fbe5865b888798c4b4
AliAkbar <aliakbarranjdoost@gmail.com>
2023-11-06 11:27:03 +0330

    Pathway 1 - Activity 7 - 3. State in Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "dev.aliakbar.basicstatecodelab"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "dev.aliakbar.basicstatecodelab"
--- app/src/main/java/dev/aliakbar/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/dev/aliakbar/basicstatecodelab/MainActivity.kt
@@ -1,16 +1,14 @@
 
 package dev.aliakbar.basicstatecodelab
 
+import WellnessScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.aliakbar.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity()
@@ -25,27 +23,9 @@ class MainActivity : ComponentActivity()
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier)
-{
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview()
-{
-    BasicStateCodelabTheme {
-        Greeting("Android")
     }
 }
