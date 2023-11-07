8bf69b6b906c09e2d85cdc79fc21db46d13af17e
AliAkbar <aliakbarranjdoost@gmail.com>
2023-11-07 11:21:20 +0330

    Pathway 1 - Activity 7 - 7. Remember in Composition

--- app/src/main/java/dev/aliakbar/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/dev/aliakbar/basicstatecodelab/WaterCounter.kt
@@ -1,5 +1,8 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import dev.aliakbar.basicstatecodelab.WellnessTaskItem
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier)
@@ -19,14 +23,36 @@ fun WaterCounter(modifier: Modifier = Modifier)
 
         if (count > 0)
         {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask)
+            {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
+
             // This text is present if the button has been clicked
             // at least once; absent otherwise
             Text(text = "You`ve had $count glasses")
         }
 
-        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp), enabled = count<10)
+        Row(modifier = Modifier.padding(top = 8.dp))
         {
-            Text(text = "Add one")
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            )
+            {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            {
+                Text(text = "Clear water count")    
+            }
         }
     }
 }
