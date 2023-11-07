90c32b52c7dabf41166a0d52c56092e3458fa85f
AliAkbar <aliakbarranjdoost@gmail.com>
2023-11-07 12:14:00 +0330

    Pathway 1 - Activity 7 - 10. Work with lists

--- app/src/main/java/dev/aliakbar/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/dev/aliakbar/basicstatecodelab/WellnessScreen.kt
@@ -1,8 +1,13 @@
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import dev.aliakbar.basicstatecodelab.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier)
 {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
---
+++ app/src/main/java/dev/aliakbar/basicstatecodelab/WellnessTask.kt
@@ -0,0 +1,3 @@
+package dev.aliakbar.basicstatecodelab
+
+data class WellnessTask(val id: Int, val label: String)
